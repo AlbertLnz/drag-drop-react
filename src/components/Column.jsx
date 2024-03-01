f307a106cd7926a75fd8ad38905e5232f0381632
AlbertLnz <albert2000_lanza@hotmail.com>
2024-03-01 13:05:04 +0100

    Highlight the indicator when we move a card

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -17,18 +17,58 @@ const Column = ({ title, headingColor, column, cards, setCards }) => {
   // Colorize the column onDragOver:
   const handleDragOver = (e) => {
     e.preventDefault()
+    HiglightIndicator(e)
     setActive(true)
   }
 
   // Decolorize the column onDragLeave:
   const handleDragLeave = () => {
+    clearHiglights()
     setActive(false)
   }
 
   // Finalize colorize when drag is completed (onDrop):
   const handleDragEnd = () => {
+    clearHiglights()
     setActive(false)
   }
+
+  // Higlight the indicator
+  const HiglightIndicator = (e) => {
+    const indicators = getIndicators()
+    clearHiglights(indicators)
+    const el = getNearestIndicator(e, indicators)
+    el.element.style.opacity = "1"
+  }
+  const getIndicators = () => {
+    return Array.from(document.querySelectorAll(`[data-column="${column}"]`))
+  }
+  const getNearestIndicator = (e, indicators) => {
+    const DISTANCE_OFFSET = 50
+    const el = indicators.reduce(
+      (closest, child) => {
+        const box = child.getBoundingClientRect()
+        const offset = e.clientY - (box.top + DISTANCE_OFFSET)
+
+        if(offset < 0 && offset > closest.offset) {
+          return { offset: offset, element: child }
+        } else {
+          return closest
+        }
+
+      }, {
+        offset: Number.NEGATIVE_INFINITY,
+        element: indicators[indicators.length - 1]
+      }
+    )
+    return el
+  }
+  const clearHiglights = (els) => {
+    const indicators = els || getIndicators()
+    indicators.forEach((i) => {
+      i.style.opacity = "0"
+    })
+  } 
 
   return (
     <div className="w-56 shrink-0">
