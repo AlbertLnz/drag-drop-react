7be5d6e49f88e34c5d07fb888b2afc12c48a05aa
AlbertLnz <albert2000_lanza@hotmail.com>
2024-03-01 09:19:42 +0100

    Board component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Board from "./components/Board"
+
 const App = () => {
   return (
-    <div>
-      <h1 className="text-4xl font-bold">Hello World!</h1>
+    <div className="h-screen w-full bg-neutral-900 text-neutral-50">
+      <Board />
     </div>
   )
 }
---
+++ src/components/Board.jsx
@@ -0,0 +1,9 @@
+const Board = () => {
+  return (
+    <div className="flex h-full w-full gap-3 overflow-scroll p-12">
+
+    </div>
+  )
+}
+
+export default Board
