7de1b14783f2ccc9629d65ade9e6e39c2f3ffc94
AlbertLnz <albert2000_lanza@hotmail.com>
2024-03-01 12:53:30 +0100

    Colorize the columns when drag a card (onDragOver, onDragLeave, onDrop)

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -14,13 +14,29 @@ const Column = ({ title, headingColor, column, cards, setCards }) => {
     assignId(card.id); // <---------------------------- USING ZUSTAND!    
   }
 
+  // Colorize the column onDragOver:
+  const handleDragOver = (e) => {
+    e.preventDefault()
+    setActive(true)
+  }
+
+  // Decolorize the column onDragLeave:
+  const handleDragLeave = () => {
+    setActive(false)
+  }
+
+  // Finalize colorize when drag is completed (onDrop):
+  const handleDragEnd = () => {
+    setActive(false)
+  }
+
   return (
     <div className="w-56 shrink-0">
       <div className="mb-3 flex items-center justify-between">
         <h3 className={`font-medium ${headingColor}`}>{title}</h3>
         <span className="rounded text-sm text-neutral-400">{filteredCards.length}</span>
       </div>
-      <div className={`h-full w-full transition-colors ${!active ? 'bg-neutral-800/50' : 'bg-neutral-800/0'}`}>
+      <div onDragOver={handleDragOver} onDragLeave={handleDragLeave} onDrop={handleDragEnd} className={`h-full w-full transition-colors ${!active ? 'bg-neutral-800/0' : 'bg-neutral-800/50'}`}>
         {filteredCards.map((card) => {
           return(
             <Card key={card.id} {...card} handleDragStart={handleDragStart}/>
